c3b8eaa92ee47c192c4690a33f609cc191d7f841
aryandwivedi-224 <aryandwivedi17thor@gmail.com>
2025-12-24 00:20:20 +0530

    Update backend and frontend configurations

--- Frontend/src/features/api/authapi.js
+++ Frontend/src/features/api/authapi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const USER_API = (import.meta.env.VITE_API_BASE_URL || "https://e-learning-management-system-l5yo.onrender.com") + "/api/v1/user";
+const USER_API = (import.meta.env.VITE_API_BASE_URL || "http://localhost:8080") + "/api/v1/user";
 
 export const authApi = createApi({
     reducerPath:"authApi",
